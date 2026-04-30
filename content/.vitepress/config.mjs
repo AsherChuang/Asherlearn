eecf3cb2cc6f05286dd15e625fd4be107bb119d2
ChuangHu07 <CHH429@pitt.edu>
2026-04-30 19:23:54 -0400

    init my quartz site

---
+++ content/.vitepress/config.mjs
@@ -0,0 +1,89 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "Asher的知识库",
+  description: "记录Asher所学",
+  themeConfig: {
+    outline: {
+      level: [2, 3],  // 加这一行，显示二级和三级标题
+    },
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '编程', link: '/code/index' },
+      { text: '玄学', link: '/daoism/index' },
+      { text: '吉他', link: '/guitar/index' },
+      { text: '其他', link: '/others/index'}
+    ],
+
+    sidebar: {
+      '/code/': [
+        {
+          text: '编程',
+          collapsed: false,
+          items: [
+            { text: '目录', link: '/code/index' },
+            {
+              text: 'Computer Architecture',
+              collapsed: true,
+              items: [
+                { text: '目录', link: '/code/computer-architecture/index' },
+                { text: '基本概念', link: '/code/computer-architecture/basic' },
+              ]
+            }
+          ]
+        }
+      ],
+      '/daoism/': [
+        {
+          text: '玄学',
+          collapsed: false,
+          items: [
+            { text: '目录', link: '/daoism/index' },
+            {
+              text: '奇门遁甲',
+              collapsed: true,
+              items: [
+                { text: '目录', link: '/daoism/qimen/index' },
+                {
+                  text: '案例',
+                  collapsed: true,
+                  items: [
+                    { text: '舅舅问11年佳女健康', link: '/daoism/qimen/examples/jiujiu-question' },
+                  ]
+                }
+              ]
+            },
+            { text: '六爻', link: '/daoism/liuyao/index', 
+              collapsed: true,
+              items: [
+                {text: '六爻取数', link: '/daoism/liuyao/points/SelectANumber'}
+              ]
+            },
+          ]
+        }
+      ],
+      '/guitar/': [
+        {
+          text: '吉他',
+          collapsed: false,
+          items: [
+            { text: '目录', link: '/guitar/index' },
+          ]
+        }
+      ],
+      '/others/': [
+        {
+          text: '其他',
+          collapsed: false,
+          items: [
+            { text: '目录', link: '/others/index' },
+          ]
+        }
+      ],
+    },
+
+    search: {
+      provider: 'local'
+    },
+  }
+})
