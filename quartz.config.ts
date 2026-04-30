b2844e5676bc0945c86a733bafbff74dad8c9a94
ChuangHu07 <CHH429@pitt.edu>
2026-04-30 19:33:59 -0400

    set baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "asherchuang.github.io/Asherlearn",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
