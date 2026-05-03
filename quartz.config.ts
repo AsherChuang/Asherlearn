71da4bda59f8fce8a28e8e50067e223de8fc286f
ChuangHu07 <CHH429@pitt.edu>
2026-05-03 01:13:51 -0400

    Quartz sync: May 3, 2026, 1:13 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "asherlearn.com",
-    ignorePatterns: ["private", "templates", ".obsidian","Excalidraw","**/attachments/**"],
+    ignorePatterns: ["private", "templates", ".obsidian","**/attachments/**"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
